9ac4b38512daa7b6cf730010adf897365246a373
Bryan Smith <bryan.smith@uottawa.ca>
2015-05-14 13:17:24 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ssp
-Simple Python web server (learning project).
+Simple web server for the "Summer Server Project."
+
+Note - this is not meant to, in its current state, be used in anything other than a local testing environment. This is an application being developed as a learning exercise.
---
+++ ssp.py
@@ -0,0 +1,23 @@
+import SimpleHTTPServer
+import SocketServer
+import sys
+
+PORT = 8888
+
+# http://stackoverflow.com/a/25375077
+class SSPHTTPHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def log_message(self, format, *args):
+        print("[%s]: %s ==> %s" % (self.log_date_time_string(), self.client_address[0], format%args))
+
+
+class sspserver():
+	
+	def __init__(self):
+		Handler = VanHTTPHandler
+		httpd = SocketServer.TCPServer(("", PORT), Handler)
+		print "Serving on port", PORT
+		httpd.serve_forever()
+		
+if __name__ == "__main__":
+	s = sspserver()
