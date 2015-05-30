3fcef2f99d89f7e2f4d271e8eae38b8964dd66cd
Bryan Smith <bryan.smith@uottawa.ca>
2015-05-30 11:23:52 -0400

    Test page and some tweaks.

--- ssp.py
+++ ssp.py
@@ -7,12 +7,13 @@ import SocketServer
 import sys
 
 PORT = 8888
+SSP_VERSION = "ssp/"
 
 # http://stackoverflow.com/a/25375077
 class SSPHTTPHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
 	# Set the server version
-	SimpleHTTPServer.SimpleHTTPRequestHandler.server_version = "ssp/0.1"
+	SimpleHTTPServer.SimpleHTTPRequestHandler.server_version = SSP_VERSION
 
 	def log_message(self, format, *args):
 		"""
@@ -21,6 +22,7 @@ class SSPHTTPHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		
 		# Print log message per request to the web server
 		print("[%s]: %s ==> %s" % (self.log_date_time_string(), self.client_address[0], format%args))
+		print(args[1])
 
 class sspserver():
 	
@@ -33,11 +35,12 @@ class sspserver():
 		self.config.read("config.ssp")
 		
 		PORT = int(self.config.get("setup", "port"))
+		SSP_VERSION = self.config.get("setup", "ssp_version")
 		
 		try:
 			Handler = SSPHTTPHandler
 			httpd = SocketServer.TCPServer(("", PORT), Handler)
-			print "Serving on port", PORT
+			print "Serving on port " + str(PORT)
 			httpd.serve_forever()
 		except KeyboardInterrupt:
 			sys.exit(0)
