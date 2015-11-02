143766835ba47b5e6e1b99db47da1643209bce23
Bryan Smith <vansmith@Bryans-MacBook-Pro.local>
2015-11-01 20:34:50 -0500

    Minor tweaks

--- ssp.py
+++ ssp.py
@@ -288,7 +288,7 @@ class sspserver():
 
 		if useLinuxComplex == "True":
 			# http://stackoverflow.com/a/1267524
-			IP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]			
+			IP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 		else:
 			# Thank to http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib for the IP tips.
 			if usehost == "False":
