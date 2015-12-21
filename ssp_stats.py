74fb546b54d2740c9a1df69fc7f6ed6225dda6eb
Bryan Smith <vansmith@Bryans-MacBook-Pro.local>
2015-12-21 18:19:55 -0500

    Added some prelim chart functionality to ssp_stats

--- ssp_stats.py
+++ ssp_stats.py
@@ -16,6 +16,13 @@ class ssp_stats():
         statsDBLocation = self.config.get("stats", "location")
         statsDB = anydbm.open(statsDBLocation, "c")
 
+        showDaily = self.config.get("stats", "show_daily_requests")
+
+        visualize = self.config.get("stats", "visual_requests")
+        visualize_bars = ""
+
+        date_time = time.strftime("%H-%M-%S_%d-%m-%Y")
+
         # http://www.tutorialspoint.com/python/python_command_line_arguments.htm
         # if sys.argv[1] == "reset":
             # opt = raw_input(":: Are you sure that you want to reset the statistics (y/n)? ")
@@ -30,7 +37,6 @@ class ssp_stats():
             option = sys.argv[1]
             if option == "export_csv":
                 output = "key, value\n"
-                date_time = time.strftime("%H-%M-%S_%d-%m-%Y")
                 for keys in sorted(statsDB.keys()):
                     output += "%s, %s\n" % (keys, statsDB[keys])
                 output_csv = open("%s/ssp_csv_%s.csv" % (self.config.get("stats", "output_csv"), date_time), "w")
@@ -40,8 +46,54 @@ class ssp_stats():
         except IndexError:
             try:
                 for keys in sorted(statsDB.keys()):
-                    #print(" :: %s: %s" % (keys, statsDB[keys]))
-                    print(" %s=%s" % (keys, statsDB[keys]))
+                    if showDaily == "False":
+                        if keys[:8] == "requests":
+                            pass
+                        else:
+                            print(" %s=%s" % (keys, statsDB[keys]))
+                    else:
+                        print(" %s=%s" % (keys, statsDB[keys]))
+
+                    if visualize == "True":
+                        visualize_bars += "\n                                         [\"%s\", %s]," % (keys, statsDB[keys])
+                if visualize == "True":
+                    # https://developers.google.com/chart/interactive/docs/quick_start#how-about-a-bar-chart
+                    visualize_html = """
+                        <html>
+                            <head>
+                                <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+                                <script type="text/javascript">
+                                    google.load("visualization", "1.0", {"packages":["corechart"]});
+                                    google.setOnLoadCallback(drawGraph);
+                                    function drawGraph() {
+                                        var data = new google.visualization.DataTable();
+                                        data.addColumn("string", "requests");
+                                        data.addColumn("number", "Requests");
+
+                                        data.addRows([
+                                            %s
+                                        ]);
+
+                                        var options = {
+                                            "title": "Number of Requests by Date, Browser and OS",
+                                            "width": 1000,
+                                            "height": 2000
+                                        };
+
+                                        var chart = new google.visualization.BarChart(document.getElementById("chart_div"));
+                                        chart.draw(data, options);
+                                    }
+                                </script>
+                            </head>
+                            <body>
+                                <div id="chart_div"></div>
+                            </body>
+                        </html>
+                    """ % visualize_bars[:-1] # http://stackoverflow.com/a/15478161
+                    f = open("visualize_html_%s.html" % date_time, "w")
+                    f.writelines(visualize_html)
+                    f.close()
+
             except KeyError:
                 print(":: No data.")
 
